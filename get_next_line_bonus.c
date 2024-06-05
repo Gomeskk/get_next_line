48eb4ba241f23710bb2d0601d9bac93989a14999
Gomeskk <gomesjoao08@hotmail.com>
2024-06-05 12:07:05 +0100

    testing

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: joafaust <joafaust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 21:30:46 by joafaust          #+#    #+#             */
-/*   Updated: 2024/06/05 10:19:40 by joafaust         ###   ########.fr       */
+/*   Updated: 2024/06/05 12:06:32 by joafaust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,3 +35,31 @@ char	*get_next_line(int fd)
 	}
 	return (line);
 }
+
+/*int main()
+{
+	int fd[3];
+
+	fd[0] = open("test1.txt", O_RDONLY);
+	fd[2] = open("test3.txt", O_RDONLY);
+	fd[1] = open("test2.txt", O_RDONLY);
+	char *test1 = get_next_line(fd[0]);
+	char *test2 = get_next_line(fd[1]);
+	char *test3 = get_next_line(fd[2]);
+	while(test1 || test2 || test3)
+	{
+		printf("test1: %s\n", test1);
+		printf("test2: %s\n", test2);
+		printf("test3: %s\n", test3);
+		test1 = get_next_line(fd[0]);
+		test2 = get_next_line(fd[1]);
+		test3 = get_next_line(fd[2]);
+	}
+	free(test1);
+	free(test2);
+	free(test3);
+
+	close(fd[0]);
+	close(fd[1]);
+	close(fd[2]);
+}*/
